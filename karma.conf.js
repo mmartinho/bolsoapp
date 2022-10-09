86de45ccca6fd30f7dad5322b0f7c2b493894aa1
marcus.martinho <marcus.martinho@tjam.jus.br>
2022-10-09 15:45:07 -0400

    Instalação do github pages deploy

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/alurapic'),
+      dir: require('path').join(__dirname, './coverage/bolsoapp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL : 'https://mmartinho.github.com/alurapic',
+  apiURL : 'https://mmartinho.github.com/bolsoapp',
   logURL : 'https://mmartinho.github.com/serverlog',
   cloudinaryName : 'mmartinhocloudinary'
 };
